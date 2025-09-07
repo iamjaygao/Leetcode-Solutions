bb860dfb5820aac4abdc2e1cbcf708f498895f4e
iamjaygao <102485789+iamjaygao@users.noreply.github.com>
2025-09-07 16:46:48 -0400

    Time: 180 ms (7.86%), Space: 32.1 MB (7.09%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -3,18 +3,7 @@ class Solution:
         """
         Finds the kth largest element using a min-heap of size k
         """
-        min_heap = []
-        for num in nums:
-            # push the num to the min_heap
-            heapq.heappush(min_heap, num)
-
-            # if heap size exceed k, remove the smallest element
-            if len(min_heap) > k:
-                heapq.heappop(min_heap)
-
-        return min_heap[0]
-
-
+        return heapq.nlargest(k,nums)[-1]
         
 
 
