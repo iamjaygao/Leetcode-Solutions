3bfedd4313b9b6289e67f5e4a516acbf5740338f
iamjaygao <102485789+iamjaygao@users.noreply.github.com>
2025-09-07 20:24:44 -0400

    Time: 75 ms (66.16%), Space: 36.1 MB (5.43%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,12 +1,45 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        """
-        Finds the kth largest element using a min-heap of size k
-        """
-        heapq.heapify(nums)
-        for i in range(len(nums) - k):
-            heapq.heappop(nums)
-        return nums[0]
-        
+        n = len(nums)
+        target_index = n - k #the index of kth largest element in sorted order
+
+        def quickselect(left, right):
+            """
+            helper function to perform the QuickSelect algorithm
+            """
+
+            # randomly choose a pivot index and swap with the leftmost element
+            pivot_index = random.randint(left, right)
+            nums[left], nums[pivot_index] = nums[pivot_index], nums[left]
+            pivot = nums[left]
+
+            # partitioning
+            # i moves from left + 1 to right, j moves from right to left
+
+            i ,j = left+1, right
+            while i <= j:
+                # find element from left that is smaller than pivot
+                # find element from right to left which is greater than pivot
+
+                if nums[i] > pivot and nums[j] < pivot:
+                    nums[i], nums[j] = nums[j], nums[i]
+                
+                if nums[i] <= pivot:
+                    i += 1
+                if nums[j] >= pivot:
+                    j -= 1
+            # place the pivot in its correct positon
+            nums[left], nums[j] = nums[j], nums[left]
+
+            # check if we found the target
+            if j == target_index:
+                return nums[j]
+            
+            elif j < target_index:
+                return quickselect(j+1, right) # search in the right partition
+            
+            else:
+                return quickselect(left, j-1) # search in the left partiton
 
 
+        return quickselect(0, n-1)
