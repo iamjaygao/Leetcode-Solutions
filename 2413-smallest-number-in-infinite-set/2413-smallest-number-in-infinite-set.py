7099953689ded5f543323d9c8a48ceffabb1f641
iamjaygao <102485789+iamjaygao@users.noreply.github.com>
2025-09-09 11:25:11 -0400

    Time: 28 ms (67.57%), Space: 12.9 MB (43.98%) - LeetHub

--- 2413-smallest-number-in-infinite-set/2413-smallest-number-in-infinite-set.py
+++ 2413-smallest-number-in-infinite-set/2413-smallest-number-in-infinite-set.py
@@ -1,28 +1,34 @@
 class SmallestInfiniteSet(object):
 
     def __init__(self):
-        self.heap = []
-        self.in_heap = set()
-        self.current_min = 1
+        self.heap = []                 #min_heap for numbersa dded via addback
+        self.in_heap = set()           # fast existence checking for heap numbers
+        self.current_min = 1           # next avaiable natural number
 
     def popSmallest(self):
         """
         :rtype: int
         """
-        if self.heap:
+        # priority: return smallest from addedd-back numbers
+        if self.heap:       
             smallest = heapq.heappop(self.heap)
             self.in_heap.remove(smallest)
             return smallest
-
-        smallest = self.current_min
-        self.current_min += 1
-        return smallest
+        
+        # if no added_back numbers, retuen the next natural number
+        else:
+            smallest = self.current_min
+            self.current_min += 1           # move to next natural number
+            return smallest
 
     def addBack(self, num):
         """
         :type num: int
         :rtype: None
         """
+        # only procrss if:
+        # 1, num < current_min( was previously popped)
+        # 2, num not in heap (avoid dupicates)
         if num < self.current_min and num not in self.in_heap:
             heapq.heappush(self.heap, num)
             self.in_heap.add(num)
