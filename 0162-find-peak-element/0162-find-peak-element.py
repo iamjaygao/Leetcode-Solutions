a272a6eeb1257d21c59ea99c4968d856e664efc0
iamjaygao <102485789+iamjaygao@users.noreply.github.com>
2025-09-10 11:59:26 -0400

    Time: 0 ms (100%), Space: 12.6 MB (51.38%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -5,12 +5,11 @@ class Solution(object):
         :rtype: int
         """
         left, right = 0 ,len(nums)-1
-
         while left < right:
             mid = left + (right - left) // 2
-            if nums[mid] > nums[mid + 1]:
-                right = mid
+            if nums[mid] < nums[mid + 1]:
+                left = mid +1
             else:
-                left = mid + 1
+                right = mid 
         return left
-                    
+
