e74ad0769fc443b6cf258cc4f210f9196b346b31
iamjaygao <102485789+iamjaygao@users.noreply.github.com>
2025-09-11 15:23:47 -0400

    Time: 0 ms (100%), Space: 17.7 MB (47.46%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -5,21 +5,12 @@ class Solution:
             return 0
         if n == 1 or n == 2:
             return 1
-        
-        # initialize dp array of size n+1 
-        # initialize with zeros for clean state and defined values
-        dp = [0]* (n + 1)
+        a,b,c = 0, 1, 1
 
-        # set the known base values
-        dp[0] = 0
-        dp[1] = 1
-        dp[2] = 1
-
-        # bottom-up computation
-        for i in range(3,n+1):
-            dp[i] = dp[i-1]+ dp[i-2] + dp[i-3]
-        
-        # return the nth tribonacci number
-        return dp[n]
-
+        for i in range(3, n+1):
+            new_val = a + b + c
+            a = b
+            b = c
+            c = new_val
+        return c
         
