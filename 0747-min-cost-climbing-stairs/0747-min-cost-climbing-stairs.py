84bb7fac2fa6c8d232a1daa55597aff878128e88
iamjaygao <102485789+iamjaygao@users.noreply.github.com>
2025-09-11 16:24:38 -0400

    Time: 3 ms (72.44%), Space: 18 MB (46.68%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,26 +1,14 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         n = len(cost)
+        if n <= 1:
+            return 0 if n == 0 else cost[0]
 
-        # handle the edge cases
-        if n == 0:
-            return 0
-        if n == 1:
-            return cost[0] # only one step, must pay to climb from it
-        
-        # initialize DP array
-        # size n+1 to store costs for steps 0 through n
-        # initialize wit zeros for clean state
-        dp = [0] * (n + 1)
-
-        # set base case, note that reaching there costs 0, but leave there to higher step cost cost[i]
-        dp[0] = 0       #cost 0 to reach step 0 
-        dp[1] = 0       #cost 0 to reach step 1
+        prev2 = 0
+        prev1 = 0
 
         for i in range(2, n+1):
-            from_prew = dp[i-1] + cost[i-1]
-            from_two_back = dp[i-2] + cost[i-2]
-            dp[i] = min(from_prew, from_two_back)
-
-        # return the cost to reach the top (step n)
-        return dp[n]
+            current = min(prev1 + cost[i-1], prev2 + cost[i-2])
+            prev2 = prev1
+            prev1 = current
+        return prev1
