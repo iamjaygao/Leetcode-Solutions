8eafddc1eaff9d375aa6d86c762af931364afc46
iamjaygao <102485789+iamjaygao@users.noreply.github.com>
2025-09-15 17:59:24 -0400

    Time: 0 ms (100%), Space: 17.9 MB (12.86%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -5,6 +5,8 @@ class Solution:
             return 0
         if n == 1:
             return nums[0]
+        if n == 2:
+            return max(nums[0], nums[1])
 
         dp = [0] * n
         dp[0] = nums[0]
