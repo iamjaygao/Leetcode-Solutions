07dc99f1c16be71e6b9543d3d8676e3e3de51e61
iamjaygao <102485789+iamjaygao@users.noreply.github.com>
2025-09-16 15:32:51 -0400

    Time: 6 ms (53.24%), Space: 18.6 MB (22.2%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,10 +1,18 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        # initialize DP array with zeros
         dp = [0] * (n+1)
-        for i in range(1, n+1):
-            if i % 2 == 0:
-                dp[i] = dp[i//2]
-            else:
-                dp[i] = dp[i-1] + 1
 
+        # Iterate through all number from 1 to n
+        for i in range(1, n+1):
+            # key insight:
+            # dp[i] = dp[i >> 1] + (i & 1)
+            #
+            # i >> 1: right shift by 1 (equvialent to i // 2)
+            # i & 1: returns 1 if i is ood, 0 if even
+            #
+            # For even numbers: i & 1 = 0, so dp[i] = dp[i//2] 
+            # For odd numbers: i & 1 = 1, so dp[i] = dp[i//2] + 1
+            dp[i] = dp[i>>1] + (i & 1)
         return dp
+
