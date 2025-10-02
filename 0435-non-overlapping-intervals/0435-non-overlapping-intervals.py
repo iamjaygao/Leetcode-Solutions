9b81668c27ff86221cb66cf7ffc13941cb86f738
iamjaygao <102485789+iamjaygao@users.noreply.github.com>
2025-10-02 14:18:32 -0400

    Time: 96 ms (48.58%), Space: 51.4 MB (21.91%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -3,15 +3,16 @@ class Solution:
         if not intervals:
             return 0
         count = 0
-        intervals.sort(key = lambda x: x[1])
+        intervals.sort(key = lambda x : x[1])
         current_pos = intervals[0][1]
+
         for i in range(1, len(intervals)):
             start, end = intervals[i]
             if start < current_pos:
                 count += 1
             else:
                 current_pos = end
-        return count
+        return count 
 
-
+ 
         
