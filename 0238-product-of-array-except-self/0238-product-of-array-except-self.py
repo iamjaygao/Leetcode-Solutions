00c320991006d599144f1971a1576294e41f4402
iamjaygao <102485789+iamjaygao@users.noreply.github.com>
2025-10-19 15:50:56 -0400

    Time: 17 ms (92.17%), Space: 23.3 MB (80.31%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,17 +1,22 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        """
+        1: We are given an integer array, 
+        2: return the answer, such that answer[i] = product all element except itself
+        """
         n = len(nums)
-        answer = [1] * n
+        result = [1] * n
+        # step 1: get the product of left side of the i
         left = 1
         for i in range(n):
-            answer[i] = left
+            result[i] = left
             left *= nums[i]
-
+        # step2: right part
         right = 1
-        for i in range(n-1,-1,-1):
-            answer[i] *= right
+        for i in range(n-1, -1, -1):
+            result[i] *= right
             right *= nums[i]
-        return answer
+        
+        return result 
 
-            
-
+ 
