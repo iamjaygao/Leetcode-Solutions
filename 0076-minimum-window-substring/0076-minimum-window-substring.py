ad584c7c2267312c0bcc5c4e8f6903bc1ef0d683
iamjaygao <102485789+iamjaygao@users.noreply.github.com>
2025-10-21 11:45:53 -0400

    Time: 99 ms (40.04%), Space: 18.1 MB (76.74%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,33 +1,32 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        # return the smallest contiguous substring,such that every character in t is includede in it
-        # including duplicates, 
-
-        m, n = len(s), len(t)   # get the size of s and n
-        count = Counter(t)      # et all elements in t and its frequency
-        windows = {}            # ceeate a dict to save the element
-        found = 0               # how many requeired char been found so far
+        count = Counter(t)
+        window = Counter()
+        found = 0
         res = [-1, -1]
-        res_len = float("inf")
+        len_res = float("inf")
         left = 0
         for right, char in enumerate(s):
             if char in count:
-                windows[char] = windows.get(char, 0) + 1
-            if char in count and windows[char] == count[char]:
-                found += 1
+                window[char] = window.get(char, 0) + 1
+                if window[char] == count[char]:
+                    found += 1
             while found == len(count):
-                if (right - left + 1) < res_len:
-                    res_len = right - left + 1
-                    res =[left, right]
+                if right - left + 1 < len_res:
+                    len_res = right - left + 1
+                    res = [left, right]
                 left_char = s[left]
                 if left_char in count:
-                    windows[left_char] -= 1
-                if left_char in count and windows[left_char] < count[left_char]:
-                    found -= 1
+                    window[left_char] -= 1
+                    if window[left_char] < count[left_char]:
+                        found -= 1
                 left += 1
-        l, r = res
-        return s[l :r+1] if res_len != float("inf") else ""    
-                
+            l ,r = res
+        return s[l: r+1] if len_res != float("inf") else ""
+          
+
+
+
 
 
 
